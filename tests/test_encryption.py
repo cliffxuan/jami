badc679f8f4c586ddeaf8d09c3e956ec0935829a
Cliff Xuan <cliff.xuan@gmail.com>
2024-01-21 18:27:31 +0000

    init

---
+++ tests/test_encryption.py
@@ -0,0 +1,7 @@
+from jami.encryption import password_decrypt, password_encrypt
+
+
+def test_encrypt_decrypt():
+    cypher = password_encrypt(b"hello world", "mypassword")
+    print(cypher)
+    assert password_decrypt(cypher, "mypassword") == b"hello world"
